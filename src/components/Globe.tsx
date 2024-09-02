b5e71c5cfb82ad6ce5e0a631005058ee25ce9c94
Llyr4472 <mr.prashant2077@gmail.com>
2024-09-02 16:07:34 +0545

    simple globe

---
+++ src/components/Globe.tsx
@@ -0,0 +1,52 @@
+import { useRef, useEffect } from "react";
+import Globe, { GlobeMethods } from "react-globe.gl";
+import * as THREE from "three";
+
+const CLOUDS_IMG_URL = "/assets/clouds.png";
+const CLOUDS_ALT = 0.004;
+const CLOUDS_ROTATION_SPEED = -0.006; // deg/frame
+
+const MyGlobe = () => {
+  const globeRef = useRef<GlobeMethods>();
+
+  useEffect(() => {
+    const globe = globeRef.current;
+    if (globe) {
+
+      // Set up auto-rotation
+      globe.controls().autoRotate = true;
+      globe.controls().autoRotateSpeed = 0.5;
+
+      // Add clouds
+      const cloudsGeometry = new THREE.SphereGeometry(
+        globe.getGlobeRadius() * (1 + CLOUDS_ALT),
+        75,
+        75
+      );
+      new THREE.TextureLoader().load(CLOUDS_IMG_URL, (cloudsTexture) => {
+        const cloudsMaterial = new THREE.MeshPhongMaterial({
+          map: cloudsTexture,
+          transparent: true,
+        });
+        const clouds = new THREE.Mesh(cloudsGeometry, cloudsMaterial);
+        globe.scene().add(clouds);
+
+        function rotateClouds() {
+          clouds.rotation.y += (CLOUDS_ROTATION_SPEED * Math.PI) / 180;
+          requestAnimationFrame(rotateClouds);
+        }
+        rotateClouds();
+      });
+    }
+  }, []);
+
+  return (
+    <Globe
+      ref={globeRef}
+      globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+      bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+    />
+  );
+};
+
+export default MyGlobe;
