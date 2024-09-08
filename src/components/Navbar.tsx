fd352e0c20fefb6a054de8dbc8052e71dfa427ee
Llyr4472 <mr.prashant2077@gmail.com>
2024-09-08 20:14:46 +0545

    Add login button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,144 +1,145 @@
-import './../global.css'
+import "./../global.css";
+
 import { Link } from "react-router-dom";
 
 interface Alert {
-    type: string;
-    location: string;
-    time: string;
-  }
+  type: string;
+  location: string;
+  time: string;
+}
 
-  interface LeftSidebarProps {
-    onSearch: (searchTerm: string) => void;
-    onFilter: (type: string) => void;
-    onAlertClick: (alert: Alert) => void;
-  }
+interface LeftSidebarProps {
+  onSearch: (searchTerm: string) => void;
+  onFilter: (type: string) => void;
+  onAlertClick: (alert: Alert) => void;
+}
 
-  const Navbar: React.FC<LeftSidebarProps> = ({
-    onSearch,
-    onFilter,
-    onAlertClick,
-  }) => {
-    return (
-      <nav
-        className="navbar navbar-expand-lg fixed-top px-4 my-3 mx-3 rounded-pill"
-        style={{
-          backgroundColor: "transparent",
-          backdropFilter: "blur(10px)",
-          boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-          fontFamily: "monospace",
-        }}
-      >
-        <div className="container-fluid">
-          <a
-            className="navbar-brand fw-bold hover-effect"
-            href="#"
-            style={{ color: "#4d4dff" }}
-          >
-            Disalert
-          </a>
-          <button
-            className="navbar-toggler hover-effect"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
-              <li className="nav-item hover-effect">
-                <Link
-                  className="nav-link active text-light"
-                  aria-current="page"
-                  to="/home"
-                >
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item  hover-effect">
-                <a className="nav-link text-light" href="#">
-                  Alerts
-                </a>
-              </li>
-              <li className="nav-item dropdown  hover-effect">
-                <a
-                  className="nav-link dropdown-toggle text-light"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Disaster Types
-                </a>
-
-                <ul
-                  className="dropdown-menu rounded-5"
-                  style={{
-                    backgroundColor: "transparent",
-                    backdropFilter: "blur(10px)",
-                    fontFamily: "monospace",
-                  }}
-                >
-                  <li>
-                    <a
-                      className="dropdown-item text-light hover-effect rounded-5"
-                      href="#"
-                    >
-                      Earthquake
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="dropdown-item text-light hover-effect rounded-5"
-                      href="#"
-                    >
-                      Flood
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="dropdown-item text-light hover-effect rounded-5"
-                      href="#"
-                    >
-                      Hurricane
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="dropdown-item text-light hover-effect rounded-5"
-                      href="#"
-                    >
-                      Wildfire
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a
-                      className="dropdown-item text-light hover-effect rounded-5"
-                      href="#"
-                    >
-                      All Disasters
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item  hover-effect">
-                <a
-                  className="nav-link text-light"
-                  href="https://www.redcross.org/get-help.html"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Emergency Resources
-                </a>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
+const Navbar: React.FC<LeftSidebarProps> = ({
+  onSearch,
+  onFilter,
+  onAlertClick,
+}) => {
+  return (
+    <nav
+      className="navbar navbar-expand-lg fixed-top px-4 my-3 mx-3 rounded-pill"
+      style={{
+        backgroundColor: "transparent",
+        backdropFilter: "blur(10px)",
+        boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+        fontFamily: "monospace",
+      }}
+    >
+      <div className="container-fluid">
+        <a
+          className="navbar-brand fw-bold hover-effect"
+          href="#"
+          style={{ color: "#4d4dff" }}
+        >
+          Disalert
+        </a>
+        <button
+          className="navbar-toggler hover-effect"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item hover-effect">
+              <Link
+                className="nav-link active text-light"
+                aria-current="page"
+                to="/home"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item hover-effect">
+              <a className="nav-link text-light" href="#">
+                Alerts
+              </a>
+            </li>
+            <li className="nav-item dropdown hover-effect">
+              <a
+                className="nav-link dropdown-toggle text-light"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Disaster Types
+              </a>
+              <ul
+                className="dropdown-menu rounded-5"
+                style={{
+                  backgroundColor: "transparent",
+                  backdropFilter: "blur(10px)",
+                  fontFamily: "monospace",
+                }}
+              >
+                <li>
+                  <a
+                    className="dropdown-item text-light hover-effect rounded-5"
+                    href="#"
+                  >
+                    Earthquake
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="dropdown-item text-light hover-effect rounded-5"
+                    href="#"
+                  >
+                    Flood
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="dropdown-item text-light hover-effect rounded-5"
+                    href="#"
+                  >
+                    Hurricane
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="dropdown-item text-light hover-effect rounded-5"
+                    href="#"
+                  >
+                    Wildfire
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a
+                    className="dropdown-item text-light hover-effect rounded-5"
+                    href="#"
+                  >
+                    All Disasters
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item hover-effect">
+              <a
+                className="nav-link text-light"
+                href="https://www.redcross.org/get-help.html"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Emergency Resources
+              </a>
+            </li>
+          </ul>
+          <div className="d-flex align-items-center">
+            <form className="d-flex me-2" role="search">
               <input
                 className="form-control me-2 bg-light text-dark border-0 rounded-pill hover-effect search-bar"
                 type="search"
@@ -147,7 +148,7 @@ interface Alert {
                 style={{ fontFamily: "monospace", width: "250px" }}
               />
               <button
-                className="btn rounded-pill hover-effect"
+                className="btn btn-outline-light rounded-pill hover-effect"
                 type="submit"
                 onClick={() => {
                   const searchInput = document.querySelector(
@@ -157,32 +158,28 @@ interface Alert {
                     onSearch(searchInput.value);
                   }
                 }}
-                style={{
-                  backgroundColor: "rgba(77, 77, 255, 0.5)",
-                  color: "#4d4dff",
-                  fontFamily: "monospace",
-                  transition: "transform 0.1s",
-                }}
               >
                 Search
               </button>
             </form>
-            <Link to="/login">
-              <button
-                className="btn btn-outline-light rounded-pill hover-effect ms-2"
-                style={{
-                  fontFamily: "monospace",
-                  transition: "transform 0.1s",
-                }}
-                onClick={() => {}}
-              >
-                Login
-              </button>
-          </Link>
           </div>
+          <Link to="/login" className="btn rounded-circle hover-effect ms-3">
+            <img
+              src="/assets/user.svg"
+              alt="Login"
+              className="user-icon"
+              style={{
+                width: "40px",
+                height: "40px",
+                backgroundColor: "#4d4dff",
+                borderRadius: "100%",
+                padding: "2px",
+              }}
+            />
+          </Link>
         </div>
-      </nav>
-    );
-
-  }
-  export default Navbar
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
