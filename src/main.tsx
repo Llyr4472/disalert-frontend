05c935f1e69b6ae168285cef4c1811926ca419e4
Llyr4472 <mr.prashant2077@gmail.com>
2024-10-03 21:59:32 +0545

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import App from "./App";
 import * as React from 'react';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
+    <Router>
       <App />
-    </HashRouter>
+    </Router>
   </React.StrictMode>
 );
