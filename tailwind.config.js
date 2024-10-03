246cd48ed859627e8b4ca39464234aaac599f6ee
Llyr4472 <mr.prashant2077@gmail.com>
2024-10-03 20:11:12 +0545

    build(deps): update 2 files and create 1 file

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        redPantone: "#e63946ff",
+        honeydew: "#F1FAEE",
+        nonPhotoBlue: "#a8dadcff",
+        cerulean: "#457b9dff",
+        berkeleyBlue: "#1d3557ff",
+      },
+    },
+  },
+  plugins: [],
+}
+
