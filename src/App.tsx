aafdc6a3ef6f82dcdaf8f4e1adc11f36b19516f5
Llyr4472 <mr.prashant2077@gmail.com>
2024-10-03 20:52:07 +0545

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
+  Navigate,
   } from "react-router-dom";
 import Signup from "./components/Signup";
 
@@ -43,6 +44,7 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/home" element={
             <>
               <div id="app-root">
